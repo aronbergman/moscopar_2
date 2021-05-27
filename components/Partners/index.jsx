61191a826d9886745d59943b0d80dabf81566d11
Aron Bergman <brgmn@icloud.com>
2021-05-27 15:12:36 +0300

    Правка очереди логотипов

--- components/Partners/index.jsx
+++ components/Partners/index.jsx
@@ -14,15 +14,15 @@ import image10 from '../../public/partners/10.png'
 
 const Partners = () => {
     const partners = [
-        {image: image1},
-        {image: image2},
         {image: image3},
-        {image: image4},
-        {image: image5},
-        {image: image6},
-        {image: image7},
-        {image: image8},
         {image: image9},
+        {image: image6},
+        {image: image1},
+        {image: image4},
+        {image: image2},
+        {image: image7},
+        {image: image5},
+        {image: image8},
         {image: image10},
     ]
 

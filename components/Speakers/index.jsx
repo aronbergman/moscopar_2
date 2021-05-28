1177f6d29520b6507da255b9b7cf5e74f8e5b717
Aron Bergman <brgmn@icloud.com>
2021-05-28 21:52:44 +0300

    fix(stylesheet): bugs from designer

--- components/Speakers/index.jsx
+++ components/Speakers/index.jsx
@@ -39,81 +39,95 @@ const Speakers = () => {
 
             <Slider {...settings}>
                 <div>
-
-
                     <div className={styles.Squires}>
-                        <Square photo={katlama} color={'#FF005C'}/>
-                        <Square
-                            nameFirst={' Кристин'}
-                            nameSecond={'Катлама'}
-                            description={'Профессор Университета Сорбонны, Института общественного здравоохранения и эпидемиологии им. Пьера Луи Пастера, Руководитель исследовательской группы по антиретровирусной стратегии Французского национального агентства по исследованию ВИЧ, основатель и Президент AFRAVIH, член правления EACS'}
-                            arrow={'right'}
-                            color={'#FF005C'}
-                        />
-                        <Square photo={otran} color={'#002381'} />
-                        <Square
-                            nameFirst={' Брижит'}
-                            nameSecond={'Отран'}
-                            description={'Профессор иммунологии в Университетской больнице Питье-Сальпетриер в Париже, Университета Пьера и Марии Кюри, Франция'}
-                            arrow={'right'}
-                            color={'#002381'}
-                            textColor={'white'}
-                        />
-                        <Square
-                            nameFirst={'Анн-Женевьев '}
-                            nameSecond={'Марселин'}
-                            description={'Профессор медицины в Университете Сорбонны в Париже, Франция, клинический вирусолог в больнице Питье-Сальпетриер'}
-                            arrow={'left'}
-                            color={'#265CDD'}
-                            nameColor={'#00CFFF'}
-                            textColor={'white'}
-                        />
-                        <Square photo={marselin} color={'#00CFFF'}/>
-                        <Square
-                            nameFirst={'Роман '}
-                            nameSecond={'Палих'}
-                            description={'Врач отделения инфекционных заболеваний Больницы Питье-Сальпетриер'}
-                            arrow={'left'}
-                            color={'#00CFFF'}/>
-                        <Square photo={palih} color={'#fff'}/>
+                        <div className={styles.Mobile}>
+                            <Square photo={katlama} color={'#FF005C'}/>
+                            <Square
+                                nameFirst={' Кристин '}
+                                nameSecond={' Катлама '}
+                                description={'профессор Университета Сорбонны, Института общественного здравоохранения и эпидемиологии им. Пьера Луи Пастера, руководитель исследовательской группы по антиретровирусной стратегии Французского национального агентства по исследованию ВИЧ, основатель и президент AFRAVIH, член правления EACS'}
+                                arrow={'right'}
+                                color={'#FF005C'}
+                            />
+                        </div>
+                        <div className={styles.Mobile}>
+                            <Square photo={otran} color={'#002381'}/>
+                            <Square
+                                nameFirst={' Брижит '}
+                                nameSecond={' Отран '}
+                                description={'профессор иммунологии в Университетской больнице Питье-Сальпетриер в Париже, Университета Пьера и Марии Кюри, Франция'}
+                                arrow={'right'}
+                                color={'#002381'}
+                                textColor={'white'}
+                            />
+                        </div>
+                        <div className={styles.Mobile}>
+                            <Square
+                                nameFirst={' Анн-Женевьев '}
+                                nameSecond={' Марселин '}
+                                description={'профессор медицины в Университете Сорбонны в Париже, Франция, клинический вирусолог в больнице Питье-Сальпетриер'}
+                                arrow={'left'}
+                                color={'#265CDD'}
+                                nameColor={'#00CFFF'}
+                                textColor={'white'}
+                            />
+                            <Square photo={marselin} color={'#00CFFF'}/>
+                        </div>
+                        <div className={styles.Mobile}>
+                            <Square
+                                nameFirst={' Роман '}
+                                nameSecond={' Палих '}
+                                description={'врач отделения инфекционных заболеваний Больницы Питье-Сальпетриер'}
+                                arrow={'left'}
+                                color={'#00CFFF'}/>
+                            <Square photo={palih} color={'#fff'}/>
+                        </div>
                     </div>
                 </div>
                 <div>
                     <div className={styles.Squires}>
-                        <Square photo={pronin} color={'#FF005C'}/>
-                        <Square
-                            nameFirst={' Пронин'}
-                            nameSecond={'Александр Юрьевич'}
-                            description={'Главный врач Московского Областного Центра СПИД. главный внештатный специалист Министерства здравоохранения Московской области по проблемам диагностики и лечения ВИЧ-инфекции, к.м.н.'}
-                            arrow={'right'}
-                            color={'#FF005C'}
-                        />
-                        <Square photo={drobyshevskaya} color={'#002381'}/>
-                        <Square
-                            nameFirst={' Дробышевская'}
-                            nameSecond={'Елена Владимировна'}
-                            description={'Заместитель главного врача по медицинской части Московского Областного Центра СПИД, врач высшей категории'}
-                            arrow={'right'}
-                            color={'#002381'}
-                            textColor={'white'}
-                        />
-                        <Square
-                            nameFirst={'Орлова-Морозова '}
-                            nameSecond={'Елена Александровна '}
-                            description={'Заведующая амбулаторно-поликлиническим отделением Московского Областного Центра СПИД, к.м.н.'}
-                            arrow={'left'}
-                            color={'#265CDD'}
-                            nameColor={'#00CFFF'}
-                            textColor={'white'}
-                        />
-                        <Square photo={orlovaMorozova} color={'#00CFFF'}/>
-                        <Square
-                            nameFirst={'Каминский '}
-                            nameSecond={'Григорий Дмитриевич'}
-                            description={'Руководитель отдела инфекционной патологии НМИЦ ФПИ, врач-инфекционист, доктор медицинских наук'}
-                            arrow={'left'}
-                            color={'#00CFFF'}/>
-                        <Square photo={kaminsky} color={'#fff'}/>
+                        <div className={styles.Mobile}>
+                            <Square photo={pronin} color={'#FF005C'}/>
+                            <Square
+                                nameFirst={' Пронин '}
+                                nameSecond={' Александр Юрьевич '}
+                                description={'главный врач Московского Областного Центра СПИД. главный внештатный специалист Министерства здравоохранения Московской области по проблемам диагностики и лечения ВИЧ-инфекции, к.м.н.'}
+                                arrow={'right'}
+                                color={'#FF005C'}
+                            />
+                        </div>
+                        <div className={styles.Mobile}>
+                            <Square photo={drobyshevskaya} color={'#002381'}/>
+                            <Square
+                                nameFirst={' Дробышевская '}
+                                nameSecond={' Елена Владимировна '}
+                                description={'заместитель главного врача по медицинской части Московского Областного Центра СПИД, врач высшей категории'}
+                                arrow={'right'}
+                                color={'#002381'}
+                                textColor={'white'}
+                            />
+                        </div>
+                        <div className={styles.Mobile}>
+                            <Square
+                                nameFirst={' Орлова-Морозова '}
+                                nameSecond={' Елена Александровна '}
+                                description={'заведующая амбулаторно-поликлиническим отделением Московского Областного Центра СПИД, к.м.н.'}
+                                arrow={'left'}
+                                color={'#265CDD'}
+                                nameColor={'#00CFFF'}
+                                textColor={'white'}
+                            />
+                            <Square photo={orlovaMorozova} color={'#00CFFF'}/>
+                        </div>
+                        <div className={styles.Mobile}>
+                            <Square
+                                nameFirst={' Каминский '}
+                                nameSecond={' Григорий Дмитриевич '}
+                                description={'руководитель отдела инфекционной патологии НМИЦ ФПИ, врач-инфекционист, доктор медицинских наук'}
+                                arrow={'left'}
+                                color={'#00CFFF'}/>
+                            <Square photo={kaminsky} color={'#fff'}/>
+                        </div>
                     </div>
                 </div>
             </Slider>

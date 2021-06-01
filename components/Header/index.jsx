9c0ba81c73fe9f54d0e319e1b6cad150cf898bf6
Aron Bergman <brgmn@icloud.com>
2021-06-01 18:56:17 +0500

    feat(translate): add ru and fr lenguage

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { menu } from "../../menu";
 import Burger from "./Burger";
-import { i18n, Link, withTranslation } from './../../i18n'
+import { i18n, withTranslation } from './../../i18n'
 import styles from './Header.module.scss'
 
 const Header = ({ t }) => {
-    console.log(i18n)
     return (
         <>
             <div className={styles.Text}>21–22 сентября <span>онлайн</span></div>
@@ -20,9 +19,9 @@ const Header = ({ t }) => {
                             <a key={index} href={item.link} className={styles.MenuItems}>{t(`header.${item.name}`)}</a>
                         ))
                     }
-                    {/*<div className={styles.MenuItems} onClick={() => i18n.changeLanguage(i18n.language === 'fr' ? 'ru' : 'fr')}>*/}
-                    {/*    {t('header.change-locale')}*/}
-                    {/*</div>*/}
+                    <div className={styles.MenuItems} onClick={() => i18n.changeLanguage(i18n.language === 'fr' ? 'ru' : 'fr')}>
+                        {t('header.change-locale')}
+                    </div>
                 </div>
             </div>
         </>

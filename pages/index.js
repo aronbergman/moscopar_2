e251d10d542723af8bb94980a59030d852842001
Aron Bergman <brgmn@icloud.com>
2021-06-02 18:55:35 +0300

    feat(i18n): add translate

--- pages/index.js
+++ pages/index.js
@@ -65,8 +65,11 @@ export default function Home() {
         <div className={styles.container}>
             <Head>
                 <title>MOSCOPAR 2021</title>
-                <meta name="description" content="MOSCOPAR – русско-французская научно-практическая конференция. 24–25 июня 2021"/>
+                <meta name="description"
+                      content="MOSCOPAR – русско-французская научно-практическая конференция. 24–25 июня 2021"/>
                 <link rel="icon" href="/favicon.ico"/>
+                <meta property="og:image" content="/logo.png"/>
+                <meta name="twitter:image" content="/logo.png"/>
                 <link rel="stylesheet" type="text/css" charSet="UTF-8"
                       href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
                 <link rel="stylesheet" type="text/css"
@@ -93,13 +96,14 @@ export default function Home() {
                 <Footer/>
             </main>
 
-            <div className={[styles.TapTop, 'tap-top'].join(' ')} style={goingUp ? { display: 'block' } : { display: 'none' }}
+            <div className={[styles.TapTop, 'tap-top'].join(' ')}
+                 style={goingUp ? { display: 'block' } : { display: 'none' }}
                  onClick={tapToTop}>
                 <svg id="Layer_1"
-                     style={{transform: 'rotate(180deg)'}}
+                     style={{ transform: 'rotate(180deg)' }}
                      data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 143.68 181.43">
                     <path className="cls-1"
-                          style={{fill:'#ff005c'}}
+                          style={{ fill: '#ff005c' }}
                           d="M140.05,91.44a11.44,11.44,0,0,0-16.17.58l-40,43.06V12.37A12.22,12.22,0,0,0,72.59,0a11.9,11.9,0,0,0-9,3.24A12.06,12.06,0,0,0,59.85,12V135.11L19.81,92A11.44,11.44,0,0,0,3.05,107.59l68.79,73.84,68.79-73.84A11.44,11.44,0,0,0,140.05,91.44Z"/>
                 </svg>
             </div>
